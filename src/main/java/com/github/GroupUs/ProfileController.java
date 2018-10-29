c19cfdb7f1c74984efa60373c66ee61bd7f9221b
haofang-eric <fangh9416@gmail.com>
2018-10-28 21:00:04 -0400

    profile controller

--- src/main/java/com/github/GroupUs/ProfileController.java
+++ src/main/java/com/github/GroupUs/ProfileController.java
@@ -1,43 +1,74 @@
 package com.github.GroupUs;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.List;
+import com.github.GroupUs.factory.ServiceFactory;
+import static com.github.GroupUs.Main.userId;
+import com.github.GroupUs.vo.EventInfo;
 
 public class ProfileController {
 
-
+    //    String userId = getText();
     //    list of joined event Obj = ServiceFactory.getIUserServiceInstance().getPostedEvent(userId);
     //    list of posted event Obj = ServiceFactory.getIUserServiceInstance().getJoinedEvent(userId);
     //    user Obj = ServiceFactory.getIUserServiceInstance().get(userId);
 
-
     @FXML
-    private ListView postList;
+    private TableView<EventInfo> postTable ;
     @FXML
-    private ListView joinList;
+    private TableView<EventInfo> joinTable;
+    @FXML
+    private Label outputarea;
 
-// given a user id !
-    public void initialize(URL location, ResourceBundle resources){
+    //
+    // given a user id !
+    public void initialize() throws Exception{
+        userId = "trypost123@columbia.edu";  //only for now
+        List<EventInfo> postList = ServiceFactory.getIUserServiceInstance().getPostedEvent(userId);
+        List<EventInfo> joinList = ServiceFactory.getIUserServiceInstance().getJoinedEvent(userId);
+        ObservableList<EventInfo> postitem = FXCollections.observableList(postList);
+        ObservableList<EventInfo> joinitem = FXCollections.observableList(joinList);
+
+        TableColumn<EventInfo, String> categoryColumn1 = new TableColumn<>("Category");
+        categoryColumn1.setCellValueFactory(new PropertyValueFactory<>("category"));
+
+        TableColumn<EventInfo, String> subjectColumn1 = new TableColumn<>("Subject");
+        subjectColumn1.setCellValueFactory(new PropertyValueFactory<>("subject"));
+
+        TableColumn<EventInfo, String> locationColumn1 = new TableColumn<>("Location");
+        locationColumn1.setCellValueFactory(new PropertyValueFactory<>("location"));
+        postTable.setPlaceholder(new Label("No content"));
+        postTable.setItems(postitem);
+        postTable.getColumns().addAll(categoryColumn1,subjectColumn1,locationColumn1);
+
+        TableColumn<EventInfo, String> categoryColumn2 = new TableColumn<>("Category");
+        categoryColumn2.setCellValueFactory(new PropertyValueFactory<>("category"));
+
+        TableColumn<EventInfo, String> subjectColumn2 = new TableColumn<>("Subject");
+        subjectColumn2.setCellValueFactory(new PropertyValueFactory<>("subject"));
+
+        TableColumn<EventInfo, String> locationColumn2 = new TableColumn<>("Location");
+        locationColumn2.setCellValueFactory(new PropertyValueFactory<>("location"));
+        joinTable.setPlaceholder(new Label("No content"));
+        joinTable.setItems(joinitem);
+        joinTable.getColumns().addAll(categoryColumn2,subjectColumn2,locationColumn2);
 
     }
 
-
-    public void getPostEvent(){
-
-    }
-
-    public void getJoinEvent(){
-
-    }
 
 
     @FXML
@@ -54,6 +85,20 @@ public class ProfileController {
         Stage formerStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
         formerStage.setScene(new Scene(newRoot, 600, 400));
         // user id set to initial ?
+    }
+
+    @FXML
+    private void showFull(MouseEvent mouseEvent) {
+        EventInfo itemSelected ;
+        if (postTable.getSelectionModel().getSelectedItem() != null ){
+            itemSelected = postTable.getSelectionModel().getSelectedItem();
+        } else{
+            itemSelected = joinTable.getSelectionModel().getSelectedItem();
+        }
+        outputarea.setText("Category:  "+itemSelected.getCategory()+"\n"+"Subject:  "+itemSelected.getSubject()+"\n"+
+                "Location:  "+itemSelected.getLocation()+"\n"+
+                "start:  "+itemSelected.getStart()+"\n"+"end:  "+itemSelected.getEnd()+"\n"+"Memo:  "+itemSelected.getMemo()+"\n"+
+                "Description:  "+itemSelected.getDescription());
     }
     // finish profile and search/join interface
     // adjust status interface
